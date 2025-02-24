d967148ee30d2afca7c7b612e6a83ae139957eca
youssefmahesh <youssef.mahish4@gmail.com>
2025-02-24 02:47:51 -0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const settings = {
   packname: 'Knight',
   author: 'Bot',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botOwner: 'Youssef Al-Thaqafi', // Your name
+  ownerNumber: '6287768582623', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
